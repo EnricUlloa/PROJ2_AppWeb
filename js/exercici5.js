f61a09d75566b6c1c97657f288e2af20a91f4057
Enric <2024_enric.ulloa@iticbcn.cat>
2025-03-07 15:11:41 +0100

    CodigoWeb

---
+++ js/exercici5.js
@@ -0,0 +1,9 @@
+/** Show the "exercise 5" **/
+function showExercise5() {
+    document.getElementsByClassName('row')[0].style.display = 'none';
+    document.getElementsByClassName('row')[1].style.display = 'none';
+    document.getElementsByClassName('row')[2].style.display = 'none';
+    document.getElementsByClassName('row')[3].style.display = 'none';
+    document.getElementsByClassName('row')[4].style.display = 'none';
+    document.getElementsByClassName('row')[5].style.display = 'flex';
+}
